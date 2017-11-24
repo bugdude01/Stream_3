4cbac86d2016af6bffd044119bd7c1f75859b7d7
bugdude01 <fafrhodes@yahoo.co.uk>
2017-11-24 12:38:06 +0000

    adding requirements and settings for staging purposes.

---
+++ settings/staging.py
@@ -0,0 +1,35 @@
+from base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Paypal environment variables
+PAYPAL_NOTIFY_URL = 'https://291e2d8f.ngrok.io/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'aaron@codeinstitute.net'
+
+SITE_URL = 'https://your-heroku-app.herokuapp.com'
+ALLOWED_HOSTS.append('your-heroku-app.herokuapp.com')
+
+# Log DEBUG information to the console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
