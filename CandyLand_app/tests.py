5a05715e3cd129a90581c51030cbefeb365fa9ca
bugdude01 <fafrhodes@yahoo.co.uk>
2017-11-24 12:00:18 +0000

    add test files

--- CandyLand_app/tests.py
+++ CandyLand_app/tests.py
@@ -2,5 +2,56 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .views import get_home
+from django.core.urlresolvers import resolve
+from django.shortcuts import render_to_response
+from accounts.models import User
 
 # Create your tests here.
+
+
+"""
+class HomePageTest(TestCase):
+    def test_home_page_resolves(self):
+        home_page = resolve('/')
+        self.assertEqual(home_page.func, get_home)
+
+    def test_home_page_status_code_is_ok(self):
+        home_page = self.client.get('/')
+        self.assertEqual(home_page.status_code, 200)
+
+    def test_check_content_is_correct(self):
+        home_page = self.client.get('/')
+        self.assertTemplateUsed(home_page, "home.html")
+        home_page_template_output = render_to_response("home.html").content
+        self.assertEqual(home_page.content, home_page_template_output)
+
+"""
+
+
+class HomePageTest(TestCase):
+    def setUp(self):
+        super(HomePageTest, self).setUp()
+        self.user = User.objects.create(username='testuser')
+        self.user.set_password('letmein')
+        self.user.save()
+
+    def test_login(self):
+        login = self.client.login(username='testuser', password='letmein')
+        self.assertTrue(login)
+
+    def test_home_page_uses_home_view(self):
+        home_page = resolve('/')
+        self.assertEqual(home_page.func, get_home)
+
+    def test_home_page_uses_home_template(self):
+        home_page = self.client.get('/')
+        self.assertTemplateUsed(home_page, "home.html")
+
+    def test_home_page_logged_in_content(self):
+        self.client.login(username='testuser', password='letmein')
+        home_page = self.client.get('/')
+
+        home_page_template_output = render_to_response(
+            "home.html", {'user': self.user}).content
+        self.assertEquals(home_page.content, home_page_template_output)
--- accounts/tests.py
+++ accounts/tests.py
@@ -2,5 +2,26 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+
+
+class SimpleTest(TestCase):
+    def test_adding_something_simple(self):
+        self.assertEqual(1 + 2, 3)
+
+    def test_adding_something_isnt_equal(self):
+        self.assertNotEqual(1 + 2, 4)
+
+
+class CustomUserTest(TestCase):
+    def test_manager_create(self):
+        user = User.objects._create_user(None, "test@test.com",
+                                         "password",
+                                         False, False)
+        self.assertIsNotNone(user)
+
+        with self.assertRaises(ValueError):
+            user = User.objects._create_user(None, None, "password",
+                                             False, False)
