f25e746eb14c8b25c691de7452a9bec5d714a98e
bugdude01 <fafrhodes@yahoo.co.uk>
2017-11-28 14:20:12 +0000

    tidy some HTML and include images in products

--- CandyLand/settings.py
+++ CandyLand/settings.py
@@ -80,6 +80,9 @@ TEMPLATES = [
     },
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 WSGI_APPLICATION = 'CandyLand.wsgi.application'
 
 
--- CandyLand/urls.py
+++ CandyLand/urls.py
@@ -21,6 +21,8 @@ from paypal.standard.ipn import urls as paypal_urls
 from products import views as product_views
 from sweetsubs import views as sweetsubs_views
 from threads import views as forum_views
+from django.views.static import serve
+from .settings import MEDIA_ROOT
 
 
 from CandyLand_app import views
@@ -44,6 +46,7 @@ urlpatterns = [
     url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
     url(r'^paypal-return', paypal_views.paypal_return),
     url(r'^paypal-cancel', paypal_views.paypal_cancel),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 
     # Product URL's
     url(r'^products/$', product_views.all_products),
